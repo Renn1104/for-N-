71eba94c72e11c328ceb61942d3d09596eea7a04
Rendy <rendy.pramudya30@gmail.com>
2025-11-09 10:15:45 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+
+    const titles = ('I HAVE A CRUSH ON YOU').split('')
+    const titleElement = document.getElementById('title');
+    let index = 0;
+
+    function appendTitle() {
+      if (index < titles.length) {
+        titleElement.innerHTML += titles[index];
+        index++;
+        setTimeout(appendTitle, 500); // 1000ms delay
+      }
+    }
+
+    appendTitle();
+
+    clearTimeout(c);
+  }, 1000);
+};
